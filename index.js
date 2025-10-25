addb170e1aed0a9bbb0199f43281468d69a0418f
sanjay-2003-ss <ssanjaysuresh2003@gmail.com>
2025-10-25 13:22:27 +0530

    updated files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
-const port = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.json());
