ce1422630cb3f65a8d7c3089836f7d9330604d0d
sanjay-2003-ss <ssanjaysuresh2003@gmail.com>
2025-10-26 13:16:04 +0530

    updated files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
-
+require("dotenv").config();
 
 const app = express();
 
